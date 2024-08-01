b59d2fa9ed992396767b4ce1ec2ddc6a5b36684c
mateussiilva <mateuspython3010@gmail.com>
2024-07-31 23:03:25 -0300

    UP

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+
+@app.get("/")
+@app.get("/home")
+def home():
+    return render_template("home/index.html",title_page="Home Page")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
